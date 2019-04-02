888ebe423a6c56407d0cdcf7bed8b404d2339f6f
Аслан Нурсеитов <grooves97@gmail.com>
2019-04-02 11:58:25 +0600

    Переопределил

--- PizzaApp/Models/User.cs
+++ PizzaApp/Models/User.cs
@@ -8,6 +8,8 @@ namespace Models
 {
     public class User
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
     }
 }
--- PizzaApp/PizzaApp.DataAcces/UsersTableDataService.cs
+++ PizzaApp/PizzaApp.DataAcces/UsersTableDataService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PizzaApp.Services.Abstract
+namespace PizzaApp.DataAcces
 {
-    interface ITelegramBot
+    class UsersTableDataService
     {
 
     }
